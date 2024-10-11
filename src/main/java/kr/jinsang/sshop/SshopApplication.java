5bfb186bf53c68027049edf564e7a5be512ba3f6
Eddie Kang <20160658@gm.hannam.ac.kr>
2024-10-11 15:35:35 +0900

    init

---
+++ src/main/java/kr/jinsang/sshop/SshopApplication.java
@@ -0,0 +1,13 @@
+package kr.jinsang.sshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/jinsang/sshop/SshopApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.jinsang.sshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
