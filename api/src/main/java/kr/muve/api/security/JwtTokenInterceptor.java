0588ce6bc12aaae6a71abc3855bdf355fc59e6e7
Eddie Kang <20160658@gm.hannam.ac.kr>
2024-10-25 15:34:38 +0900

    fix: jwt토큰 어디서 오류 났는지 출력

--- api/src/main/java/kr/muve/api/security/JwtTokenInterceptor.java
+++ api/src/main/java/kr/muve/api/security/JwtTokenInterceptor.java
@@ -5,11 +5,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kr.muve.common.exception.JwtTokenInvalidException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtTokenInterceptor implements HandlerInterceptor {
 
     private final JwtTokenProvider jwtTokenProvider;
@@ -20,6 +22,7 @@ public class JwtTokenInterceptor implements HandlerInterceptor {
         String token = jwtTokenProvider.resolveToken(request);
 
         if (StringUtils.isEmpty(token) || !jwtTokenProvider.isValidToken(token)) { // fail fast
+            log.info(request.getRequestURI());
             throw new JwtTokenInvalidException("토큰이 유효하지 않습니다.");
         }
 
