2df4f83287490aa6421ccf382c18dc3427461010
ory98 <subin8360@naver.com>
2024-10-26 14:29:07 +0900

    feat: cart 로직 작업중

---
+++ api/src/main/java/kr/muve/api/cart/controller/CartRestController.java
@@ -0,0 +1,17 @@
+package kr.muve.api.cart.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/cart")
+public class CartRestController {
+
+//    @GetMapping()
+//    public CartRes
+//
+//    @PostMapping()
+//    public
+}
---
+++ api/src/main/java/kr/muve/api/cart/service/CartQueryService.java
@@ -0,0 +1,9 @@
+package kr.muve.api.cart.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CartQueryService {
+}
