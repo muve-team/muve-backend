06c99214d418e7c52ac013486250b1128c04aa1a
Eddie Kang <20160658@gm.hannam.ac.kr>
2024-10-31 22:43:31 +0900

    fix: X-Request-Id 인터셉터 추가

--- api/src/main/java/kr/muve/api/config/WebConfig.java
+++ api/src/main/java/kr/muve/api/config/WebConfig.java
@@ -1,5 +1,6 @@
 package kr.muve.api.config;
 
+import kr.muve.api.interceptor.CommonHttpRequestInterceptor;
 import kr.muve.api.security.JwtTokenInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final JwtTokenInterceptor jwtTokenInterceptor;
+    private final CommonHttpRequestInterceptor commonHttpRequestInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -31,17 +33,19 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
 //    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(jwtTokenInterceptor)  // JWT 인터셉터 등록
-//                .addPathPatterns("/**")  // 모든 경로에 대해 인터셉터 적용
-//                .excludePathPatterns(
-//                        "/",
-//                        "/user/join",
-//                        "/user/login", "/error",
-//                        "/actuator/health",
-//                        "/product/random",
-//                        "/product/detail",
-//                        "/category",
-//                        "/category/products");  // 특정 경로는 제외
-//    }
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtTokenInterceptor)  // JWT 인터셉터 등록
+                .addPathPatterns("/**")  // 모든 경로에 대해 인터셉터 적용
+                .excludePathPatterns(
+                        "/",
+                        "/user/join",
+                        "/user/login", "/error",
+                        "/actuator/health",
+                        "/product/random",
+                        "/product/detail",
+                        "/category",
+                        "/category/products");  // 특정 경로는 제외
+        registry.addInterceptor(commonHttpRequestInterceptor)  // CommonHttpRequestInterceptor 추가
+                .addPathPatterns("/**");  // 모든 경로에 적용
+    }
 }
