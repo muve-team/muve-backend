961ae092b9648e822b3db34b9e272407358884a3
Eddie Kang <20160658@gm.hannam.ac.kr>
2024-11-14 01:01:51 +0900

    fix: hottest search 및 카운트 증가 구현

--- api/src/main/java/kr/muve/api/search/controller/SearchRestController.java
+++ api/src/main/java/kr/muve/api/search/controller/SearchRestController.java
@@ -1,8 +1,10 @@
 package kr.muve.api.search.controller;
 
+import kr.muve.api.search.history.service.SearchHistoryService;
 import kr.muve.common.exception.CommonResponse;
 import kr.muve.common.service.search.ProductsSearch;
 import kr.muve.common.service.search.SearchProductsRes;
+import kr.muve.common.service.search.history.SearchHistoriesRes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,7 @@ import java.util.List;
 public class SearchRestController {
 
     private final ProductsSearch productsSearch;
+    private final SearchHistoryService searchHistoryService;
 
     @GetMapping
     public CommonResponse<SearchProductsRes> getSearchProducts(@RequestParam("keyword") String keyword,
@@ -28,6 +31,16 @@ public class SearchRestController {
         log.info("[GET] /search, keyword: {}, page: {}, size: {}", keyword, page, size);
 
         return CommonResponse.success(productsSearch.getSearchProducts(keyword, page, size));
+    }
+
+    @GetMapping("/hottest")
+    public CommonResponse<SearchHistoriesRes> getHottestSearchTerms(@RequestParam("keyword") String keyword,
+                                                                    @RequestParam(name = "page", required = false, defaultValue = "0") Integer page,
+                                                                    @RequestParam(name = "size", required = false, defaultValue = "10") Integer size) {
+
+        log.info("[GET] /search/hottest, keyword: {}, page: {}, size: {}", keyword, page, size);
+
+        return CommonResponse.success(searchHistoryService.get(size));
     }
 
 }
