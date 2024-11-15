c45cb884c5266ceb37ab9b5e44a70cb7cd4f6b3b
Eddie Kang <20160658@gm.hannam.ac.kr>
2024-11-15 19:36:28 +0900

    fix: 배송지 정보 오류 수정

--- common/src/main/java/kr/muve/common/domain/user/Address.java
+++ common/src/main/java/kr/muve/common/domain/user/Address.java
@@ -1,24 +1,26 @@
 package kr.muve.common.domain.user;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
 
 @Embeddable
 @Getter
 public class Address {
+    @Column(name = "city", nullable = false)
     private String city;
+
+    @Column(name = "street", nullable = false)
     private String street;
+
+    @Column(name = "zipcode", nullable = false)
     private String zipcode;
 
-    protected Address() {
-
-    }
+    protected Address() {}
 
     public Address(String city, String street, String zipcode) {
         this.city = city;
         this.street = street;
         this.zipcode = zipcode;
     }
-
-
 }
